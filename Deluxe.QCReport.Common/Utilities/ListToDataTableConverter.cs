131bca8819183ef09d8c569c00ed5d84ad76f99e
IKoomson1803 <isaac.koomson@bydeluxe.com>
2022-09-26 18:14:57 +0100

    Banijay Rights checklist printout continues...

---
+++ Deluxe.QCReport.Common/Utilities/ListToDataTableConverter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Data;
+using System.ComponentModel;
+
+namespace Deluxe.QCReport.Common.Utilities
+{
+    public class ListToDataTableConverter
+    {
+        public DataTable ToDataTable<T>(List<T> items)
+        {
+            DataTable dataTable = new DataTable(typeof(T).Name);
+            //Get all the properties
+            PropertyInfo[] Props = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+            foreach (PropertyInfo prop in Props)
+            {
+                //Setting column names as Property display names or names 
+                var attribute = prop.GetCustomAttributes(typeof(DisplayNameAttribute), true).Cast<DisplayNameAttribute>().FirstOrDefault();
+                string displayName = attribute.DisplayName;
+
+                if (!string.IsNullOrWhiteSpace(displayName))
+                {
+                    dataTable.Columns.Add(displayName);
+                }
+                else
+                {
+                    dataTable.Columns.Add(prop.Name);
+                }
+
+                
+               
+            }
+            foreach (T item in items)
+            {
+                var values = new object[Props.Length];
+                for (int i = 0; i < Props.Length; i++)
+                {
+                    //inserting property values to datatable rows
+                    values[i] = Props[i].GetValue(item, null);
+                }
+                dataTable.Rows.Add(values);
+            }
+            //put a breakpoint here and check datatable
+            return dataTable;
+        }
+
+    }
+}
