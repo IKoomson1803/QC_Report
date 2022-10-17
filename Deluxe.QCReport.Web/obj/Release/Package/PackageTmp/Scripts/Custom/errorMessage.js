dcfdb7e184c998e0117fe224314919655c9f4e4a
IKoomson1803 <isaac.koomson@bydeluxe.com>
2022-10-17 12:42:55 +0100

    Refactored

---
+++ Deluxe.QCReport.Web/obj/Release/Package/PackageTmp/Scripts/Custom/errorMessage.js
@@ -0,0 +1,18 @@
+﻿
+function errorAlert() {
+    $("#divMessage").html(
+        "<div><h5>Whoops! There is an error, please contact system admin.</h5></div><br/>"
+        + "<div><button onclick='closeMessageDialog()' class='btn btn-default' >Close<span class='glyphicon glyphicon-off'></span></button></div>")
+        .dialog({
+            autoOpen: true,
+            modal: true,
+            width: 500,
+            resizable: false,
+            title: "Error Message >>"
+        });
+}
+
+function closeMessageDialog() {
+    $("#divMessage").dialog("close");
+    $.unblockUI();
+}
